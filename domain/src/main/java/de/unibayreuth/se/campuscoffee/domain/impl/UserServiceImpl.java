8479dace7e637e53123ed7e0b6be2e201b356a59
Tom Zeilmann <tom-zeilmann@t-online.de>
2025-06-01 13:41:22 +0200

    add all functions to UserService

---
+++ domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/impl/UserServiceImpl.java
@@ -0,0 +1,64 @@
+package de.unibayreuth.se.campuscoffee.domain.impl;
+
+import de.unibayreuth.se.campuscoffee.domain.exceptions.PosNotFoundException;
+import de.unibayreuth.se.campuscoffee.domain.exceptions.UserNotFoundException;
+import de.unibayreuth.se.campuscoffee.domain.model.User;
+import de.unibayreuth.se.campuscoffee.domain.ports.UserDataService;
+import de.unibayreuth.se.campuscoffee.domain.ports.UserService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserDataService userDataService;
+
+    @Override
+    public void clear() {
+        userDataService.clear();
+    }
+
+    @Override
+    @NonNull
+    public List<User> findAll() {
+        return userDataService.getAll();
+    }
+
+    @Override
+    @NonNull
+    public User findById(Long id) throws UserNotFoundException {
+        return null;
+    }
+
+    @Override
+    @NonNull
+    public User findByLogin(String loginName) throws UserNotFoundException {
+        return verifyUserExists(loginName);
+    }
+
+    @Override
+    @NonNull
+    public User create(User user) {
+        return userDataService.upsert(user);
+    }
+
+    @Override
+    @NonNull
+    public User update(Long id, User user) throws UserNotFoundException {
+        if(userDataService.getById(id).isEmpty()){
+            throw new UserNotFoundException("USER with id \"" + id + "\" does not exist.");
+        }
+        else{
+            user.setId(id);
+            return userDataService.upsert(user);
+        }
+    }
+
+    private User verifyUserExists(@NonNull String loginName) throws UserNotFoundException{
+        return userDataService.getByLoginName(loginName)
+                .orElseThrow(() -> new UserNotFoundException("USER with name \"" + loginName + "\" does not exist."));
+    }
+}
--- domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/ports/UserService.java
+++ domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/ports/UserService.java
@@ -1,9 +1,26 @@
 package de.unibayreuth.se.campuscoffee.domain.ports;
 
+import de.unibayreuth.se.campuscoffee.domain.exceptions.UserNotFoundException;
+import de.unibayreuth.se.campuscoffee.domain.model.User;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Interface for the implementation of the user service that the domain module provides as a port and implements itself.
  */
 public interface UserService {
     void clear();
-    // TODO: Define user service.
+    // Define user service.
+    @NonNull
+    List<User> findAll();
+    @NonNull
+    User findById(Long id) throws UserNotFoundException;
+    @NonNull
+    User findByLogin(String loginName) throws UserNotFoundException;
+    @NonNull
+    User create(User user);
+    @NonNull
+    User update(Long id, User user) throws UserNotFoundException;
 }
