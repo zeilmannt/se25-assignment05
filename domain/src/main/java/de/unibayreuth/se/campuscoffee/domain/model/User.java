1a73cd9273ac7ed3abb97a18a6c038d66b14a006
Tom Zeilmann <tom-zeilmann@t-online.de>
2025-06-01 13:50:40 +0200

    add non null to user variables and revise user timestamps

--- domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/model/User.java
+++ domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/model/User.java
@@ -1,11 +1,14 @@
 package de.unibayreuth.se.campuscoffee.domain.model;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import javax.annotation.Nullable;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 /**
  * Domain class that stores the user metadata.
@@ -20,13 +23,17 @@ public class User implements Serializable {
     // Implement user domain class. Hint: the @Nullable annotations are important for the Lombok @Data annotation (see https://projectlombok.org/features/Data).
 
     @Nullable
-    private Instant createdAt; // set by the system on creation
+    private LocalDateTime createdAt = LocalDateTime.now(ZoneId.of("UTC")); // set on USER creation
 
     @Nullable
-    private Instant updatedAt; // set by the system on update
+    private LocalDateTime updatedAt = createdAt;
 
+    @NonNull
     private String loginName;
+    @NonNull
     private String email;
+    @NonNull
     private String firstName;
+    @NonNull
     private String lastName;
 }
