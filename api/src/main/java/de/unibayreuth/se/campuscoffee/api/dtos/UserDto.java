ec1c6a34afd1893407793e3572b72b6586f21dd5
Tom Zeilmann <tom-zeilmann@t-online.de>
2025-06-01 12:03:59 +0200

    add user information and their checks

--- api/src/main/java/de/unibayreuth/se/campuscoffee/api/dtos/UserDto.java
+++ api/src/main/java/de/unibayreuth/se/campuscoffee/api/dtos/UserDto.java
@@ -1,7 +1,12 @@
 package de.unibayreuth.se.campuscoffee.api.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.Instant;
 
 /**
  * DTO for user metadata.
@@ -11,5 +16,20 @@ import lombok.Data;
 @Builder(toBuilder = true)
 public class UserDto {
     private Long id; // id is null when creating a new task
-    // TODO: Implement user DTO.
+    // Implement user DTO.
+
+    @Pattern(regexp = "\\w+", message = "Login must contain only word characters")
+    private String login;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Size(min = 1, max = 255, message = "First name must be between 1 and 255 characters")
+    private String firstName;
+
+    @Size(min = 1, max = 255, message = "Last name must be between 1 and 255 characters")
+    private String lastName;
+
+    private Instant createdAt;
+    private Instant updatedAt;
 }
--- domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/model/User.java
+++ domain/src/main/java/de/unibayreuth/se/campuscoffee/domain/model/User.java
@@ -2,8 +2,10 @@ package de.unibayreuth.se.campuscoffee.domain.model;
 
 import lombok.Data;
 
+import javax.annotation.Nullable;
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.Instant;
 
 /**
  * Domain class that stores the user metadata.
@@ -13,6 +15,18 @@ public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Nullable
     private Long id; // null when the user has not been created yet
-    // TODO: Implement user domain class. Hint: the @Nullable annotations are important for the Lombok @Data annotation (see https://projectlombok.org/features/Data).
+    // Implement user domain class. Hint: the @Nullable annotations are important for the Lombok @Data annotation (see https://projectlombok.org/features/Data).
+
+    @Nullable
+    private Instant createdAt; // set by the system on creation
+
+    @Nullable
+    private Instant updatedAt; // set by the system on update
+
+    private String loginName;
+    private String email;
+    private String firstName;
+    private String lastName;
 }
